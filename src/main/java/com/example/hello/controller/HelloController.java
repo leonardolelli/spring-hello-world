9f6d880c028f605710a4d2353d3d2d6651f97aae
leonardo <leonardolelli97@gmail.com>
2022-06-14 11:05:00 +0200

    initial commit

---
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.hello.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.hello.service.HelloService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/hello")
+public class HelloController {
+
+	@Autowired 
+	private HelloService helloService;
+    @GetMapping
+    public String hello() {
+        return helloService.hello();
+    }
+}
