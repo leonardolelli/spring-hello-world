19b108ecaac73f01a937cf764fb7c4dbfa707cd8
leonardo <leonardolelli97@gmail.com>
2022-06-16 15:59:06 +0200

    Aggiunta autenticazione con identity server wso2

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -2,12 +2,19 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class HelloApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloApplication.class, args);
+		System.setProperty("javax.net.ssl.trustStore",
+				"C:\\WSO2\\IdentityServer\\5.11.0\\repository\\resources\\security\\client-truststore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
+
 	}
 
 }
